06963c428a15ccf53d71fc1ba08baf06f10077ac
Pavel Polyakov <me@pavelpolyakov.com>
2017-06-23 20:31:10 +0200

    babel-plugin-transform-runtime explicit

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 ssh_opt="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet";
 
 ssh $ssh_opt ocelot "cd /var/www/smart-kt.pavelpolyakov.com ;
-git pull;
+git fetch;
+git reset --hard origin/master;
 npm i;
 npm run build; ";
